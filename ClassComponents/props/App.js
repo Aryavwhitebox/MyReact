0cb9046a9d2f239ec44cd397c55c6669e5193bee
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-02-22 09:08:59 -0800

    Create App.js

---
+++ ClassComponents/props/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Child from './Child'
+class App extends React.Component {
+
+  render() {
+    return (
+      <Child name='aryav'/>
+    )
+ 
+  }
+}
+
+export default App
